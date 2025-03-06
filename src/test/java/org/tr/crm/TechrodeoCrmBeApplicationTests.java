5e7438990367698d598748f47279082a18a9a6bf
Szántó Ádám <szanto.adam1@ext.telekom.hu>
2025-03-06 15:23:24 +0100

    Initial commit

---
+++ src/test/java/org/tr/crm/TechrodeoCrmBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tr.crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechrodeoCrmBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
